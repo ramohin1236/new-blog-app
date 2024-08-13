46a89f5ed7e353134405d07ec372252b1c5679ee
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-08-13 01:14:32 -0700

    process.emv file solve

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -7,7 +7,8 @@ export const verifyToken = (req,res,next)=>{
     if(!token){
         return next(errorHandler(401, 'Unauthorized'))
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
+ 
+    jwt.verify(token, process.env.JWT_SECTET, (err, user)=>{
         if(err){
             return next(errorHandler(401,'Unauthorized'))
         }
