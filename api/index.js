1a52f535a8b87eaa38dbce2f0d67ce637c3d4fae
ramohin1236 <mohinr26@gmail.com>
2025-05-04 15:01:47 +0600

    bd

--- api/index.js
+++ api/index.js
@@ -47,7 +47,7 @@ app.use('/api/post',PostRoutes )
 app.use('/api/comment',CommentRoutes)
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
